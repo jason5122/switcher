83164dafeb99cfe4728ba62f86c6532fb25edd89
Jason Han <jjhan@vassar.edu>
2023-11-08 16:36:01 -0500

    Use transparent title bar

--- app/controller/window_controller.m
+++ app/controller/window_controller.m
@@ -3,8 +3,11 @@
 @implementation WindowController
 
 - (instancetype)initWithBounds:(CGRect)bounds {
+    // int mask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
+    //            NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable;
     int mask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
-               NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable;
+               NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable |
+               NSWindowStyleMaskFullSizeContentView;
 
     NSWindow* window = [[NSWindow alloc] initWithContentRect:bounds
                                                    styleMask:mask
@@ -13,7 +16,7 @@
 
     id bundleInfo = [[NSBundle mainBundle] infoDictionary];
     NSString* appName = [bundleInfo objectForKey:@"CFBundleName"];
-    [window setTitle:appName];
+    // [window setTitle:appName];
     [window center];
 
     // mimic Sublime Text Adaptive theme
