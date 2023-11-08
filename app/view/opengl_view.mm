a09c12a8e8878878444ede496b9666e711b9023f
Jason Han <jjhan@vassar.edu>
2023-11-08 16:49:14 -0500

    Clean up opengl_view.mm

--- app/view/opengl_view.mm
+++ app/view/opengl_view.mm
@@ -13,16 +13,12 @@ struct CppMembers {
 @implementation OpenGLView
 
 - (CVReturn)getFrameForTime:(const CVTimeStamp*)outputTime {
-    // There is no autorelease pool when this method is called
-    // because it will be called from a background thread
-    // It's important to create one or you will leak objects
     @autoreleasepool {
         [self drawView];
     }
     return kCVReturnSuccess;
 }
 
-// This is the renderer output callback function
 static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now,
                                       const CVTimeStamp* outputTime, CVOptionFlags flagsIn,
                                       CVOptionFlags* flagsOut, void* displayLinkContext) {
@@ -53,7 +49,7 @@ static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTime
 
     NSOpenGLPixelFormat* pf = [[NSOpenGLPixelFormat alloc] initWithAttributes:attribs];
     if (!pf) {
-        NSLog(@"Failed to create pixel format.");
+        log_with_type(OS_LOG_TYPE_ERROR, @"failed to create pixel format", @"capture-engine");
         return nil;
     }
 
@@ -67,11 +63,9 @@ static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTime
 - (void)initGL {
     [self.openGLContext makeCurrentContext];
 
-    // Synchronize buffer swaps with vertical refresh rate
-    GLint one = 1;
-
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    GLint one = 1;  // Synchronize buffer swaps with vertical refresh rate
     [self.openGLContext setValues:&one forParameter:NSOpenGLCPSwapInterval];
 #pragma clang diagnostic pop
 
@@ -79,18 +73,13 @@ static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTime
 }
 
 - (void)setupDisplayLink {
-    // Create a display link capable of being used with all active displays
     CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
-
-    // Set the renderer output callback function
     CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, (__bridge void*)self);
 
-    // Set the display link for the current renderer
     CGLContextObj cglContext = self.openGLContext.CGLContextObj;
     CGLPixelFormatObj cglPixelFormat = [[self pixelFormat] CGLPixelFormatObj];
     CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
 
-    // Activate the display link
     CVDisplayLinkStart(displayLink);
 }
 
@@ -114,9 +103,6 @@ static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTime
 
 - (void)drawView {
     [self.openGLContext makeCurrentContext];
-
-    // We draw on a secondary thread through the display link
-    // lock to avoid the threads from accessing the context simultaneously
     CGLLockContext(self.openGLContext.CGLContextObj);
 
     _cppMembers->capture_engine->screen_capture_video_tick();
@@ -128,9 +114,6 @@ static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTime
 }
 
 - (void)dealloc {
-    // Stop the display link BEFORE releasing anything in the view
-    // otherwise the display link thread may call into the view and crash
-    // when it encounters something that has been released
     CVDisplayLinkStop(displayLink);
     CVDisplayLinkRelease(displayLink);
 }
