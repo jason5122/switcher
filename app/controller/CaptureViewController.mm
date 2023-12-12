34d382d905a936ee53f4bdfa028059bc9198de94
Jason Han <jjhan@vassar.edu>
2023-12-11 22:43:35 -0500

    Re-enable SwiftCaptureView

--- app/controller/CaptureViewController.mm
+++ app/controller/CaptureViewController.mm
@@ -33,9 +33,8 @@
 
         // TODO: Make this work with CaptureView.
         // _captureView = [[CaptureView alloc] initWithFrame:captureFrame configuration:config];
-        // _captureView = [[SwiftCaptureView alloc] initWithFrame:captureFrame
-        // configuration:config];
-        _captureView = [[GLCaptureView alloc] initWithFrame:captureFrame configuration:config];
+        _captureView = [[SwiftCaptureView alloc] initWithFrame:captureFrame configuration:config];
+        // _captureView = [[GLCaptureView alloc] initWithFrame:captureFrame configuration:config];
         [stackView addSubview:_captureView];
 
         titleText = [NSTextField labelWithString:@""];
