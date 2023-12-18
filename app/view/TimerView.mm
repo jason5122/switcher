c8a3e41b0ed8e6a752ea2d1d921c6881a55494b6
Jason Han <jjhan@alum.vassar.edu>
2023-12-17 23:48:03 -0500

    Fix crash when closing window while switcher is open

--- app/view/TimerView.mm
+++ app/view/TimerView.mm
@@ -36,14 +36,16 @@
     if (captureAtNominalResolution) options |= kCGSWindowCaptureNominalResolution;
 
     CFArrayRef thumbnailList = CGSHWCaptureWindowList(CGSMainConnectionID(), &wid, 1, options);
-    CGImageRef frame = (CGImageRef)CFArrayGetValueAtIndex(thumbnailList, 0);
-    CFRetain(frame);
-    CFRelease(thumbnailList);
+    if (thumbnailList) {
+        CGImageRef frame = (CGImageRef)CFArrayGetValueAtIndex(thumbnailList, 0);
+        CFRetain(frame);
+        CFRelease(thumbnailList);
 
-    // https://stackoverflow.com/a/39548167/14698275
-    self.layer.minificationFilter = kCAFilterTrilinear;
-    self.layer.contents = (__bridge id)frame;
-    CFRelease(frame);
+        // https://stackoverflow.com/a/39548167/14698275
+        self.layer.minificationFilter = kCAFilterTrilinear;
+        self.layer.contents = (__bridge id)frame;
+        CFRelease(frame);
+    }
 }
 
 @end
